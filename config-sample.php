155d83d75175f53f10fd99677b63720a794d5793
Tripti Sharma <tripti.sharma@bellevuecollege.edu>
2016-07-11 14:49:37 -0700

    Added config-sample

---
+++ config-sample.php
@@ -0,0 +1,17 @@
+<?php
+/* Merchant Configuration */
+$GLOBALS['MERCHANT_LOGIN_ID'] = ""; // API login ID
+$GLOBALS['MERCHANT_TRANSACTION_KEY'] = "";  // Transaction Key
+/*
+ * *
+ * For Production value is 0
+ * For development value is 1
+ * 
+ */
+ $GLOBALS['SERVER_CODE']  =  1;
+ 
+ /* Database Configuration */
+ $GLOBALS['DATABASE_DSN'] = ''; // Data source name
+ $GLOBALS['DATABASE_USER'] = '';  // Username to connect to data source
+ $GLOBALS['DATABASE_PASSWORD'] = '';  // Password to connect to data source
+ 
